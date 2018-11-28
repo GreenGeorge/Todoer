84106ba2e4a614c36ba088bf10bffc79ecc2e41f
George Ananda <georgeananda@gmail.com>
2018-11-28 20:39:19 +0700

    Initial Commit

---
+++ Todoer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoer
+//
+//  Created by George Ananda on 28/11/18.
+//  Copyright © 2018 George Ananda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
